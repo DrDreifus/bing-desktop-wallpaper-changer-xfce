9fc4cc1237f3864767c16cc31c5c6f0d509bf25f
Sergi Aumatell <sergi.aumatell@proactiu.cat>
2016-11-27 00:46:22 +0100

    Few things

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ import getpass
 # n = Number of images predious the day given by idx
 # mkt denotes your location. e.g. en-US means United States. Put in your country code
 BingXML_URL = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-GB"
-page = urllib2.urlopen(BingXML_URL)
-BingXML = BeautifulSoup(page, "lxml")
+page        = urllib2.urlopen(BingXML_URL)
+BingXML     = BeautifulSoup(page, "lxml")
 
 # For extracting complete URL of the image
-Images = BingXML.find_all('image')
-ImageURL = "https://www.bing.com" + Images[0].url.text
+Images    = BingXML.find_all('image')
+ImageURL  = "https://www.bing.com" + Images[0].url.text
 ImageName = Images[0].startdate.text+".jpg"
 
 # All the images will be saved in '/home/[user]/Pictures/BingWallpapers/'
